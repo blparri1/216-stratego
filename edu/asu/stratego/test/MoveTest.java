a27be62d0fda26696cb8eccde4363f5cdb62e37f
dtdicker <dtdicker@asu.edu>
2017-04-12 16:31:46 -0700

    unit test ATTEMPT for testing moves - switching to manual testing

---
+++ edu/asu/stratego/test/MoveTest.java
@@ -0,0 +1,51 @@
+package edu.asu.stratego.test;
+import edu.asu.stratego.*;
+import edu.asu.stratego.gui.*;
+import edu.asu.stratego.game.*;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MoveTest {
+/*	private Server ser;
+	public static Client red;
+	public static Client blue;
+	private ClientStage cl_red;
+	private ClientStage cl_blue;
+
+	private class Spawner implements Runnable {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			MoveTest.red  = new Client();
+			red.main(null);
+			MoveTest.blue = new Client();
+			blue.main(null);
+		}
+		
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		new Thread(new Spawner());
+		Server.main(null);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("done");
+	}
+
+	@Test
+	public void test() {
+		while (red.getGame().getStatus() == GameStatus.IN_PROGRESS || red.getGame().getStatus() == GameStatus.SETTING_UP
+				|| red.getGame().getStatus() == GameStatus.WAITING_OPP ) {
+			
+		}
+		fail("Not yet implemented");
+	}
+*/
+}
