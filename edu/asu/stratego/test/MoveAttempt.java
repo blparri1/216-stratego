6838565d415e52ec2eb0dae69e2863ba56547d62
dtdicker <dtdicker@asu.edu>
2017-04-12 16:39:51 -0700

    Unable to unit test the move feature of the game, Project is too tightly coupled - switching to manual testing

--- edu/asu/stratego/test/MoveAttempt.java
+++ edu/asu/stratego/test/MoveAttempt.java
@@ -8,7 +8,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MoveTest {
+public class MoveAttempt {
 /*	private Server ser;
 	public static Client red;
 	public static Client blue;
